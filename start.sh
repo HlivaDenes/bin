948e752e679ab1e6f915f597e003c599ac7eb038
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-09-24 05:25:03 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,5 +15,7 @@ bash /home/sis/bin/install_grub-costumizer.sh
 bash /home/sis/bin/install_vmware.sh
 #~/bin/install_wireshark.sh
 bash /home/sis/bin/adduser_user.sh
+bash /home/sis/bin/adduser_win.sh
+bash /home/sis/bin/adduser_vm.sh
 bash /home/sis/bin/install_epoptes.sh
 bash /home/sis/bin/install_githubsync2crontab.sh
