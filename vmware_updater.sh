0510746049d74107bd27d64f25f94226648164ac
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-03 18:28:48 +0200

    Update vmware_updater.sh

--- vmware_updater.sh
+++ vmware_updater.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ -e vmware_updater ]
+then
+    echo "vmware_updater már lefutott"
+else
 echo "`date` ------------------------------------------------------------" >> vmware_updater
 sudo vmware-modconfig --console --install-all >> vmware_updater
 echo "`date` ************************************************************" >> vmware_updater
+fi
