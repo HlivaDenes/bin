be9333889a6d9bed6ec4201d4ed8a75614679dc3
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-07 16:55:01 +0200

    Update install_epoptes.sh

--- install_epoptes.sh
+++ install_epoptes.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ -e install_epoptes_2 ]
+if [ -e install_epoptes_3 ]
 then
-    echo "install_epoptes_2 már futott"
+    echo "install_epoptes_3 már futott"
 else
-echo "-------------install_epoptes_2---------------------" >> install_epoptes_2
-sudo apt-get install epoptes-client -y >> install_epoptes_2
-sudo rm /etc/default/epoptes-client >> install_epoptes_2
+echo "`date`-------------install_epoptes_3---------------------" >> install_epoptes_3
+sudo apt-get install epoptes-client -y >> install_epoptes_3
+sudo rm /etc/default/epoptes-client >> install_epoptes_3
 cat<<EOF> epoptes-client
 SERVER=192.168.0.100
 #PORT=789
@@ -13,7 +13,7 @@ SERVER=192.168.0.100
 WOL=g
 EOF
 sudo mv epoptes-client /etc/default/
-sudo epoptes-client -c >> install_epoptes_2
+sudo epoptes-client -c >> install_epoptes_3
 #sudo reboot
-echo "************install_epoptes_2******************" >> install_epoptes_2
+echo "`date`************install_epoptes_2******************" >> install_epoptes_3
 fi
