96ea00cca881cdfba69afdb00fc4595fa2f417bb
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-07 16:18:54 +0200

    Update mv_share.sh

--- mv_share.sh
+++ mv_share.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-sudo mv /home/user/share /home
-sudo userdel hg
-sudo userdel win
-sudo userdel user
+if [ -e mv_share ]
+then
+  echo "már futott az mv_share.sh"
+else
+echo "---------------------------------" >> mv_share
+sudo mv /home/user/share /home >> mv_share
+sudo userdel hg >> mv_share
+sudo userdel win >> mv_share
+sudo userdel user >> mv_share
+sudo userdel vm >> mv_share
+sudo rm -r /home/hg >> mv_share
+sudo rm -r /home/win >> mv_share
+sudo rm -r /home/user >> mv_share
+sudo rm -r /home/vm >> mv_share
+echom "******************************" >> mv_share
+
+
