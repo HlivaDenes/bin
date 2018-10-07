017f96ede7a7d0a5f1d9cc14ec34d3115f53595b
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-07 16:07:33 +0200

    Update github_fetch_start.sh

--- github_fetch_start.sh
+++ github_fetch_start.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-
-#if [ -e github_fetch_start ]
-#then
-#    echo "github_fetch_start már futott"
-#else
 cd /home/sis/bin && git fetch --all && git checkout --force "origin/master" && /home/sis/bin/start.sh
-
-#touch github_fetch_start
-#fi
