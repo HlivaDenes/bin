5f2db218e217d55c57be82805aefb3333b3a468d
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-07 18:28:44 +0200

    Update mv_share.sh

--- mv_share.sh
+++ mv_share.sh
@@ -3,16 +3,17 @@ if [ -e mv_share ]
 then
   echo "már futott az mv_share.sh"
 else
-echo "`date`---------------------------------" >> mv_share
-sudo mv /home/user/share /home >> mv_share
-sudo userdel hg >> mv_share
-sudo userdel win >> mv_share
-sudo userdel user >> mv_share
-sudo userdel vm >> mv_share
-sudo rm -r /home/hg >> mv_share
-sudo rm -r /home/win >> mv_share
-sudo rm -r /home/user >> mv_share
-sudo rm -r /home/vm >> mv_share
-echo "`date`******************************" >> mv_share
+  echo "`date`---------------------------------" >> mv_share
+  sudo mv /home/user/share /home >> mv_share
+  sudo userdel hg >> mv_share
+  sudo userdel win >> mv_share
+  sudo userdel user >> mv_share
+  sudo userdel vm >> mv_share
+  sudo rm -r /home/hg >> mv_share
+  sudo rm -r /home/win >> mv_share
+  sudo rm -r /home/user >> mv_share
+  sudo rm -r /home/vm >> mv_share
+  echo "`date`******************************" >> mv_share
+fi
 
 
