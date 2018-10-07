976a82efe56c550fd8434555bf72a8a201d756a7
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-07 23:29:56 +0200

    Update adduser_diak.sh

--- adduser_diak.sh
+++ adduser_diak.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -e /home/diak ]
 then
-    echo "/home/user már létezik"
+    echo "/home/diak már létezik"
 else
 sudo adduser diak --gecos "diak,,," --disabled-password
 echo "diak:diak" | sudo chpasswd
