0e17931a59c14c8b9e7eddfc72841a2c3e401a75
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-14 20:36:05 +0200

    Update install_vscode.sh

--- install_vscode.sh
+++ install_vscode.sh
@@ -6,8 +6,8 @@ else
 echo "--------------vscode_2-------------" >> install_vscode_2
 sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" >> install_vscode_2
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF >> install_vscode_2
-sudo apt update
-sudo apt -y install code  >> install_vscode_2
+sudo apt-get update
+sudo apt-get -y install code  >> install_vscode_2
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb >> install_vscode_2
 sudo dpkg -i packages-microsoft-prod.deb >> install_vscode_2
 sudo apt-get install apt-transport-https -y >> install_vscode_2
