1ca57749e6420d196460a654f922811f64187420
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-18 22:40:18 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get upgrade -y 
+#sudo apt-get upgrade -y 
 cd /home/sis/bin
 bash /home/sis/bin/passwordless_sudo.sh
 bash /home/sis/bin/install_common.sh
@@ -7,16 +7,16 @@ bash /home/sis/bin/install_desktop.sh
 bash /home/sis/bin/install_chrome.sh 
 bash /home/sis/bin/install_etcher.sh 
 #bash /home/sis/bin/install_docker.sh
-bash /home/sis/bin/install_lxd.sh
-bash /home/sis/bin/install_lxd-ui.sh
+#bash /home/sis/bin/install_lxd.sh
+#bash /home/sis/bin/install_lxd-ui.sh
 #bash /home/sis/bin/install_npm.sh
-bash /home/sis/bin/install_vscode.sh
-bash /home/sis/bin/install_grub-costumizer.sh
+#bash /home/sis/bin/install_vscode.sh
+#bash /home/sis/bin/install_grub-costumizer.sh
 #~/bin/install_virtualbox.sh
-bash /home/sis/bin/install_vmware.sh
+#bash /home/sis/bin/install_vmware.sh
 #~/bin/install_wireshark.sh
 #bash /home/sis/bin/adduser_cad.sh
-bash /home/sis/bin/install_epoptes.sh
+#bash /home/sis/bin/install_epoptes.sh
 bash /home/sis/bin/install_githubsync2crontab.sh
 #bash /home/sis/bin/vmware_updater.sh
 ####bash /home/sis/bin/mv_share.sh
