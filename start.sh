e9ad05b7cbecb49de0596fa4df22088e87a35e3f
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-18 22:33:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,17 +6,17 @@ bash /home/sis/bin/install_common.sh
 bash /home/sis/bin/install_desktop.sh
 bash /home/sis/bin/install_chrome.sh 
 bash /home/sis/bin/install_etcher.sh 
-bash /home/sis/bin/install_docker.sh
+#bash /home/sis/bin/install_docker.sh
 bash /home/sis/bin/install_lxd.sh
 bash /home/sis/bin/install_lxd-ui.sh
-bash /home/sis/bin/install_npm.sh
+#bash /home/sis/bin/install_npm.sh
 bash /home/sis/bin/install_vscode.sh
 bash /home/sis/bin/install_grub-costumizer.sh
 #~/bin/install_virtualbox.sh
 bash /home/sis/bin/install_vmware.sh
 #~/bin/install_wireshark.sh
-bash /home/sis/bin/adduser_cad.sh
+#bash /home/sis/bin/adduser_cad.sh
 bash /home/sis/bin/install_epoptes.sh
 bash /home/sis/bin/install_githubsync2crontab.sh
-bash /home/sis/bin/vmware_updater.sh
+#bash /home/sis/bin/vmware_updater.sh
 ####bash /home/sis/bin/mv_share.sh
