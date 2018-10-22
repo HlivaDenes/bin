3537e495a6f0c7302d5ed1c0d930fa7234c6d73d
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-22 23:22:22 +0200

    Update install_githubsync2crontab.sh

--- install_githubsync2crontab.sh
+++ install_githubsync2crontab.sh
@@ -1,18 +1,18 @@
 
 #!/bin/bash
-if [ -e install_githubsync2crontab ]
+if [ -e install_githubsync2crontab_2018-10-22 ]
 then
     echo "install_githubsync2crontab már futott"
 else
 cat <<EOF> githubsync
 #!/bin/bash
 cd /home/sis/bin && git fetch --all && git checkout --force "origin/master" && /home/sis/bin/start.sh
-echo `date` >>/tmp/cronhourly
+echo "`date` >>/tmp/cronhourly"
 
 EOF
 
 sudo chown root:root githubsync
 sudo chmod +x githubsync
 sudo mv githubsync /etc/cron.hourly
-touch install_githubsync2crontab
+touch install_githubsync2crontab_2018-10-22
 fi
