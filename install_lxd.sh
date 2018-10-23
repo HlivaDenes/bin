cba7137ed38878a97939efed9b2997a002e10b68
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-23 13:28:20 +0200

    Update install_lxd.sh

--- install_lxd.sh
+++ install_lxd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ -e install_lxd ]
+if [ -e install_lxd-2018-10-23 ]
 then
-    echo "install_lxd már telepítve"
+    echo "install_lxd_2018-10-23 már telepítve"
 else
-echo "`date`------------------------" >> install_lxd
-sudo apt-get install   lxd zfsutils-linux -y >> install_lxd
+echo "`date`------------------------" >> install_lxd-2018-10-23
+sudo apt-get install   lxd zfsutils-linux -y >> install_lxd-2018-10-23
 sudo adduser $USER lxd
 #newgrp lxd
-echo "`date` ==============================================" >> install_lxd
+echo "`date` ==============================================" >> install_lxd-2018-10-23
 fi
