c62ed6b7204e9f6297414593958feae15b42b5b1
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-10-27 23:01:46 +0200

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-if [ -e install_ansible_18-10-14 ]
+if [ -e install_ansible_18-10-27]
 then
   echo "install_ansible már futott"
 else
-  echo "`date`-------------------------" >> install_ansible_18-10-14
-  sudo apt-get update  >> install_ansible_18-10-14
-  sudo apt-get install software-properties-common -y >> install_ansible_18-10-14
-  sudo apt-add-repository ppa:ansible/ansible -y >> install_ansible_18-10-14
-  sudo apt-get update  >> install_ansible_18-10-14
-  sudo apt-get install ansible -y  >> install_ansible_18-10-14
-  echo "`date`**************************" >> install_ansible_18-10-14
+  echo "`date`-------------------------" >> install_ansible_18-10-27
+  sudo apt-get update >> install_ansible_18-10-27
+  sudo apt-get install software-properties-common -y >> install_ansible_18-10-27
+  sudo apt-add-repository ppa:ansible/ansible -y >> install_ansible_18-10-27
+  sudo apt-get update  >> install_ansible_18-10-27
+  sudo apt-get install ansible -y  >> install_ansible_18-10-27
+  echo "`date`**************************" >> install_ansible_18-10-27
+fi
