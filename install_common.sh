ac0ab1f1f9361f9f2ccfacc7836e32a78fdddd3c
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-11-21 12:14:19 +0100

    Update install_common.sh

--- install_common.sh
+++ install_common.sh
@@ -7,6 +7,7 @@ else
 echo "`date` ----------------" >> install_common
 sudo apt-get update >> install_common_2018-11-21
 sudo apt-get upgrade -y >> install_common_2018-11-21
+sudo apt-get dist-upgrade -y >> install_common_2018-11-21
 sudo apt-get install mc -y >> install_common_2018-11-21
 sudo apt-get install curl -y >> install_common_2018-11-21
 sudo apt-get install nmap -y >> install_common_2018-11-21
