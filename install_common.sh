3ef67949d516be1fd7a65572cd69b4f4465b133b
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-12-10 10:02:58 +0100

    Update install_common.sh

--- install_common.sh
+++ install_common.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-if [ -e install_common_2018-11-21 ]
+if [ -e install_common_2018-12-10 ]
 then
     echo "install_common már telepítve"
 else
 echo "`date` ----------------" >> install_common
-sudo apt-get update >> install_common_2018-11-21
-sudo apt-get upgrade -y >> install_common_2018-11-21
-sudo apt-get dist-upgrade -y >> install_common_2018-11-21
-sudo apt-get install mc -y >> install_common_2018-11-21
-sudo apt-get install curl -y >> install_common_2018-11-21
-sudo apt-get install nmap -y >> install_common_2018-11-21
-sudo apt-get install gdebi-core -y >> install_common_2018-11-21
-sudo apt-get install openssh-server -y >> install_common_2018-11-21
-sudo apt autoremove -y >> install_common_2018-11-21
-echo "`date` ==================" >> install_common_2018-11-21
+sudo apt-get update >> install_common_2018-12-10
+sudo apt-get upgrade -y >> install_common_2018-12-10
+sudo apt-get dist-upgrade -y >> install_common_2018-12-10
+sudo apt-get install mc -y >> install_common_2018-12-10
+sudo apt-get install curl -y >> install_common_2018-12-10
+sudo apt-get install nmap -y >> install_common_2018-12-10
+sudo apt-get install gdebi-core -y >> install_common_2018-12-10
+sudo apt-get install openssh-server -y >> install_common_2018-12-10
+sudo apt autoremove -y >> install_common_2018-12-10
+echo "`date` ==================" >> install_common_2018-12-10
 fi
