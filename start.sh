13d5e2e6015e23d5483f6cc3ca870534754eca61
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-12-18 13:44:27 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ bash /home/sis/bin/install_common.sh
 bash /home/sis/bin/install_desktop.sh
 bash /home/sis/bin/install_chrome.sh 
 #bash /home/sis/bin/install_etcher.sh 
-#bash /home/sis/bin/install_docker.sh
+bash /home/sis/bin/install_docker.sh
 #bash /home/sis/bin/install_lxd.sh
 #bash /home/sis/bin/install_lxd-ui.sh
 #bash /home/sis/bin/install_npm.sh
@@ -16,6 +16,7 @@ bash /home/sis/bin/install_chrome.sh
 #bash /home/sis/bin/install_vmware.sh
 #~/bin/install_wireshark.sh
 bash /home/sis/bin/adduser_diak.sh
+bash /home/sis/bin/setup_docker_group
 #bash /home/sis/bin/install_epoptes.sh
 bash /home/sis/bin/install_githubsync2crontab.sh
 #bash /home/sis/bin/vmware_updater.sh
