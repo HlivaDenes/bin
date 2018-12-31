464fccbb0dd9f30fabf138bcd8d92128875ab381
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-12-31 13:58:26 +0100

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,12 +4,12 @@ if [ -e $flag ]
 then
     echo '$flag már telepítve'
 else
-    echo "`date` -----start-----------" |tee $flag
+    echo "`date` -----start-----------" 
     sudo apt-get update -y |tee error && \
     sudo apt-get  install software-properties-common -y |tee $flag && \
     sudo apt-add-repository ppa:ansible/ansible -y |tee $flag && \
     sudo apt-get update -y |tee $flag && \
     sudo apt-get install ansible -y |tee $flag && \
-    sudo apt-get autoremove -y | tee $flag
+    sudo apt-get autoremove -y | tee $flag && \
     echo "`date` ========stop==========" |tee $flag
 fi
