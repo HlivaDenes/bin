f0e67e8ee1b5291dc7f81ea26a88ce6420408b4b
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-12-31 13:57:08 +0100

    Update install_common.sh

--- install_common.sh
+++ install_common.sh
@@ -4,11 +4,11 @@ if [ -e $flag ]
 then
     echo '$flag már telepítve'
 else
-    echo "`date` -----start-----------" |tee $flag
-    sudo apt-get update -y |tee error && \
+    echo "`date` -----start-----------"
+    sudo apt-get update -y && \
     sudo apt-get upgrade -y |tee $flag && \
     sudo apt-get dist-upgrade -y |tee $flag && \
     sudo apt-get install mc curl nmap gdebi-core openssh-server -y |tee $flag && \
-    sudo apt-get autoremove -y | tee $flag
+    sudo apt-get autoremove -y | tee $flag && \
     echo "`date` ========stop==========" |tee $flag
 fi
