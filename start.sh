ea090c9a4c3baecb5c004a8c3f32d6d561b75fe9
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-12-31 13:26:00 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-#sudo apt-get upgrade -y 
+
 cd /home/sis/bin
 bash /home/sis/bin/passwordless_sudo.sh
 bash /home/sis/bin/install_githubsync2crontab.sh
+bash /home/sis/bin/install_ansible.sh
 bash /home/sis/bin/install_common.sh
 bash /home/sis/bin/install_desktop.sh
 bash /home/sis/bin/install_chrome.sh 
 bash /home/sis/bin/adduser_diak.sh
 bash /home/sis/bin/install_docker.sh
 bash /home/sis/bin/setup_docker_group.sh
+bash /home/sis/bin/install_virtualbox.sh
+
 #bash /home/sis/bin/install_etcher.sh 
 #bash /home/sis/bin/install_lxd.sh
 #bash /home/sis/bin/install_lxd-ui.sh
