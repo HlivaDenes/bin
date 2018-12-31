a1499aa25e0eb9fb4db9f31cda8943f6f2ac18bf
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-12-31 13:39:18 +0100

    Update install_virtualbox.sh

--- install_virtualbox.sh
+++ install_virtualbox.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-echo "----------- install: virtualbox ----------------"
-sudo apt install build-essential -y
-sudo apt install dkms -y
-sudo apt install linux-headers-$(uname -r)
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" -y
-sudo apt-get update
-sudo apt-get virtualbox-5.2 -y
-####sudo apt-get install virtualbox-ext-pack -y
-
-
+#!/bin/sh
+flag='install_virtualbox_2018-12-31'
+if [ -e $flag ]
+then
+    echo '$flag már telepítve'
+else
+    echo "`date` -----start-----------" |tee $flag
+    sudo apt-get update -y |tee error && \
+   sudo apt-get install virtualbox libgsoap-2.8.60 virtualbox-dkms virtualbox-qt -y |tee $flag && \
+    sudo apt-get autoremove -y | tee $flag
+    echo "`date` ========stop==========" |tee $flag
+fi
